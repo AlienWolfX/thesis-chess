d369c6f7ec77bfc912f7a4715b13348e8c66f76b
Allen Cruiz <cruizallen2@gmail.com>
2024-11-13 21:12:59 +0800

    Add YOLOv8 model implementation for real-time video inference

---
+++ yolov8.py
@@ -0,0 +1,35 @@
+import cv2
+import torch
+from ultralytics import YOLO
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Load the YOLOv8 model
+model = YOLO('model/best.pt').to(device)
+
+# Initialize the webcam
+# cameraSrc = "https://{ip}/video" # IP Camera URL
+cameraSrc = 0 # Laptop Webcam
+
+cap = cv2.VideoCapture(cameraSrc)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Perform inference
+    results = model(frame)
+
+    # Display results
+    annotated_frame = results[0].plot()
+    cv2.imshow('YOLOv8 Inference', annotated_frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close windows
+cap.release()
+cv2.destroyAllWindows()
